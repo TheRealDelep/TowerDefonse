1fd90dd4234b48d9f8582ace93e34a7d2c3cfd34
thewasmans <thewasmans@gmail.Com>
2024-10-03 11:23:54 +0200

    HUD Gold, HP, show cost building select

--- scripts/UiMain.cs
+++ scripts/UiMain.cs
@@ -4,10 +4,13 @@ public partial class UiMain : Control
 {
 	[Export] private UiSelectBuilding uiSelectBuilding;
 	[Export] private Control gameOverScene;
+	[Export] private Label hudCoins;
+	[Export] private Label hudHitPoint;
 	[Export] private Timer gameOverTimer;
 	[Export] private TowerConstructionChannel towerConstructionChannel;
 	[Export] private Level level;
 	[Export] private Node main;
+	[Export] private GoldManager gold;
 	
 	public override void _Ready()
 	{
@@ -17,6 +20,12 @@ public partial class UiMain : Control
 		gameOverScene.Visible = false;
 		gameOverTimer.Timeout += () => main.GetTree().ReloadCurrentScene();
 		level.CastleDestroyed += () => ShowGameOver();
+	}
+
+	public override void _Process(double delta)
+	{
+		hudCoins.Text = (gold.CurrentGold > 1 ? "Sesterces " : "Sesterce ") + gold.CurrentGold;
+		hudHitPoint.Text = "Hewlett Packard : " + (10 - level.countEnemies);
 	}
 
 	public void ShowSelectedUI(Vector3 worldPos, Vector2 screenPos)
